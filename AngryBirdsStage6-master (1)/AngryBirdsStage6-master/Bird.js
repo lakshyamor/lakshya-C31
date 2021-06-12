d687d365543fce3d0d52d30cf71d1d5aa4a03ef0
lakshyamor <75970835+lakshyamor@users.noreply.github.com>
2021-06-12 10:54:18 +0530

    Add files via upload

---
+++ AngryBirdsStage6-master (1)/AngryBirdsStage6-master/Bird.js
@@ -0,0 +1,27 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.path = [];
+  }
+
+  display() {
+
+    super.display();
+
+    if( this.body.velocity.x > 10 && this.body.position.x > 200)
+    {
+
+    var birdPos = [this.body.position.x , this.body.position.y];
+    this.path.push(birdPos);
+    }
+
+    for(var i=0;i< this.path.length;i++){
+
+      image(this.smoke, this.path[i][0],this.path[i][1]); 
+     }
+    
+
+  }
+}
